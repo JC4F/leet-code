a2f98ff692e52b160faf002950a28f0fab258b01
JC4F <lequan12122002@gmail.com>
2024-11-09 14:21:35 +0700

    104-max-depth-binary-tree

---
+++ leet-code-75/week-3/104-max-depth-binary-tree.py
@@ -0,0 +1,23 @@
+"""
+Time: O(N) - for DFS
+Space: O(N) - for the recursive stack
+"""
+
+from typing import Optional
+
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        def dfs(root, depth):
+            if not root:
+                return depth
+            return max(dfs(root.left, depth + 1), dfs(root.right, depth + 1))
+
+        return dfs(root, 0)
