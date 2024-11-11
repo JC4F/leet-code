6c816aa461c3a2707c94c2451724fa145488d484
jc4f <lequan12122002@gmail.com>
2024-11-11 09:25:39 +0700

    572-subtree-of-another-tree

---
+++ leet-code-75/week-3/572-subtree-of-another-tree.py
@@ -0,0 +1,59 @@
+'''
+Solution 1: recursive 
+public class Solution {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (s == null) return false;
+        if (isSame(s, t)) return true;
+        return isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+    
+    private boolean isSame(TreeNode s, TreeNode t) {
+        if (s == null && t == null) return true;
+        if (s == null || t == null) return false;
+        
+        if (s.val != t.val) return false;
+        
+        return isSame(s.left, t.left) && isSame(s.right, t.right);
+    }
+}
+'''
+
+# Solution 1.1: ~ 1 ~Python
+def isMatch(self, s, t):
+    if not(s and t):
+        return s is t
+    return (s.val == t.val and 
+            self.isMatch(s.left, t.left) and 
+            self.isMatch(s.right, t.right))
+
+def isSubtree(self, s, t):
+    if self.isMatch(s, t): return True
+    if not s: return False
+    return self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
+
+#Solution 2: Merkle hashing
+# thay vì compare entire subtree thì bây h chỉ cần compare hash
+def isSubtree(self, s, t):
+    from hashlib import sha256
+    def hash_(x):
+        S = sha256()
+        S.update(x)
+        return S.hexdigest()
+        
+    def merkle(node):
+        if not node:
+            return '#'
+        m_left = merkle(node.left)
+        m_right = merkle(node.right)
+        node.merkle = hash_(m_left + str(node.val) + m_right)
+        return node.merkle
+        
+    merkle(s)
+    merkle(t)
+    def dfs(node):
+        if not node:
+            return False
+        return (node.merkle == t.merkle or 
+                dfs(node.left) or dfs(node.right))
+                    
+    return dfs(s)
