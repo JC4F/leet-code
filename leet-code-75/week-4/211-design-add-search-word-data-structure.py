e5e9d9af8c065f11423a5fec2a13bfa04c08c3cc
jc4f <lequan12122002@gmail.com>
2024-11-12 10:18:51 +0700

    211-design-add-search-word-data-structure

---
+++ leet-code-75/week-4/211-design-add-search-word-data-structure.py
@@ -0,0 +1,44 @@
+"""
+Design a data structure that supports adding new words and finding if a string matches any previously added string.
+
+Implement the WordDictionary class:
+
+WordDictionary() Initializes the object.
+void addWord(word) Adds word to the data structure, it can be matched later.
+bool search(word) Returns true if there is any string in the data structure that matches word or false otherwise. word may contain dots '.' where dots can be matched with any letter.
+
+"""
+
+
+class WordDictionary:
+    def __init__(self):
+        # Initialize your data structure here.
+        self.children = [None] * 26
+        self.isEndOfWord = False
+
+    def addWord(self, word: str) -> None:
+        # Adds a word into the data structure.
+        curr = self
+        for c in word:
+            if curr.children[ord(c) - ord("a")] == None:
+                curr.children[ord(c) - ord("a")] = WordDictionary()
+            curr = curr.children[ord(c) - ord("a")]
+
+        curr.isEndOfWord = True
+
+    def search(self, word: str) -> bool:
+        # Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter.
+        curr = self
+        for i in range(len(word)):
+            c = word[i]
+            if c == ".":
+                for ch in curr.children:
+                    if ch != None and ch.search(word[i + 1 :]):
+                        return True
+                return False
+
+            if curr.children[ord(c) - ord("a")] == None:
+                return False
+            curr = curr.children[ord(c) - ord("a")]
+
+        return curr != None and curr.isEndOfWord
