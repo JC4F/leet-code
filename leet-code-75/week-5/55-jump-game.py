9519eaf64b3ce3797210f2850bcac83de6ac03ad
jc4f <lequan12122002@gmail.com>
2024-11-22 13:32:00 +0700

    55-jump-game

---
+++ leet-code-75/week-5/55-jump-game.py
@@ -0,0 +1,35 @@
+"""
+You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise.
+
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+Example 2:
+
+Input: nums = [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+"""
+
+# backward & forward way
+"""
+bool canJump(int A[], int n) {
+    int i = 0;
+    for (int reach = 0; i < n && i <= reach; ++i)
+        reach = max(i + A[i], reach);
+    return i == n;
+}
+"""
+"""
+bool canJump(int A[], int n) {
+    int last=n-1,i,j;
+    for(i=n-2;i>=0;i--){
+        if(i+A[i]>=last)last=i;
+    }
+    return last<=0;
+}
+"""
