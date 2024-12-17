f727ad06105bba71498bcdd9074533c8af0e4ea1
jc4f <lequan12122002@gmail.com>
2024-12-17 13:38:52 +0700

    44-wild-card-matching-greedy-dfs-vs-dp

---
+++ algorithms/dynamic-programming/44-wildcard-matching.py
@@ -0,0 +1,89 @@
+"""
+Given an input string (s) and a pattern (p), implement wildcard pattern matching with support for '?' and '*' where:
+    '?' Matches any single character.
+    '*' Matches any sequence of characters (including the empty sequence).
+The matching should cover the entire input string (not partial).
+Example 1:
+
+Input: s = "aa", p = "a"
+Output: false
+Explanation: "a" does not match the entire string "aa".
+Example 2:
+
+Input: s = "aa", p = "*"
+Output: true
+Explanation: '*' matches any sequence.
+Example 3:
+
+Input: s = "cb", p = "?a"
+Output: false
+Explanation: '?' matches 'c', but the second letter is 'a', which does not match 'b'.
+"""
+
+"""
+======>// greedy solution with idea of DFS
+// starj stores the position of last * in p
+// last_match stores the position of the previous matched char in s after a *
+// e.g. 
+// s: a c d s c d
+// p: * c d
+// after the first match of the *, starj = 0 and last_match = 1
+// when we come to i = 3 and j = 3, we know that the previous match of * is actually wrong, 
+// (the first branch of DFS we take is wrong)
+// then it resets j = starj + 1 
+// since we already know i = last_match will give us the wrong answer
+// so this time i = last_match+1 and we try to find a longer match of *
+// then after another match we have starj = 0 and last_match = 4, which is the right solution
+// since we don't know where the right match for * ends, we need to take a guess (one branch in DFS), 
+// and store the information(starj and last_match) so we can always backup to the last correct place and take another guess.
+
+ bool isMatch(string s, string p) {
+        int i = 0, j = 0;
+        int m = s.length(), n = p.length();
+        int last_match = -1, starj = -1;
+        while (i < m){
+            if (j < n && (s[i] == p[j] || p[j] == '?')){
+                i++; j++;
+            }
+            else if (j < n && p[j] == '*'){
+                starj = j;
+                j++;
+                last_match = i;
+            }
+            else if (starj != -1){
+                j = starj + 1;
+                last_match++;
+                i = last_match;
+            }
+            else return false;
+        }
+        while (p[j] == '*' && j <n) j++;
+        return j == n;
+    }
+"""
+
+"""
+dp:
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        vector<vector<bool>> dp(s.size() + 1, vector(p.size() + 1, false));
+        dp[0][0] = true;
+        for (int j = 0; j < p.size() && p[j] == '*'; ++j) {
+            dp[0][j + 1] = true;
+        }
+        
+        for (int i = 1; i <= s.size(); ++i) {
+            for (int j = 1; j <= p.size(); ++j) {
+                if (p[j - 1] == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                } else {
+                    dp[i][j] = (s[i - 1] == p[j - 1] || p[j - 1] == '?') && dp[i - 1][j - 1];
+                }
+            }
+        }
+
+        return dp[s.size()][p.size()];
+    }
+};
+"""
