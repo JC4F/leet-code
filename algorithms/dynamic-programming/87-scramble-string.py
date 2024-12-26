fd703f6a286da5e5999a7ff4c8738307802b2890
jc4f <lequan12122002@gmail.com>
2024-12-26 13:20:05 +0700

    87-scramble-string

---
+++ algorithms/dynamic-programming/87-scramble-string.py
@@ -0,0 +1,89 @@
+"""
+We can scramble a string s to get a string t using the following algorithm:
+    If the length of the string is 1, stop.
+    If the length of the string is > 1, do the following:
+        Split the string into two non-empty substrings at a random index, i.e., if the string is s, divide it to x and y where s = x + y.
+        Randomly decide to swap the two substrings or to keep them in the same order. i.e., after this step, s may become s = x + y or s = y + x.
+        Apply step 1 recursively on each of the two substrings x and y.
+Given two strings s1 and s2 of the same length, return true if s2 is a scrambled string of s1, otherwise, return false.
+Example 1:
+Input: s1 = "great", s2 = "rgeat"
+Output: true
+"""
+
+"""
+class Solution {
+public:
+    bool isScramble(string s1, string s2) {
+        if(s1==s2)
+            return true;
+            
+        int len = s1.length();
+        int count[26] = {0};
+        for(int i=0; i<len; i++)
+        {
+            count[s1[i]-'a']++;
+            count[s2[i]-'a']--;
+        }
+        
+        for(int i=0; i<26; i++)
+        {
+            if(count[i]!=0)
+                return false;
+        }
+        
+        for(int i=1; i<=len-1; i++)
+        {
+            if( isScramble(s1.substr(0,i), s2.substr(0,i)) && isScramble(s1.substr(i), s2.substr(i)))
+                return true;
+            if( isScramble(s1.substr(0,i), s2.substr(len-i)) && isScramble(s1.substr(i), s2.substr(0,len-i)))
+                return true;
+        }
+        return false;
+    }
+};
+"""
+
+"""
+public class Solution {
+	public boolean isScramble(String s1, String s2) {
+		if (s1.length() != s2.length()) return false;
+		int len = s1.length();
+		/**
+		 * Let F(i, j, k) = whether the substring S1[i..i + k - 1] is a scramble of S2[j..j + k - 1] or not
+		 * Since each of these substrings is a potential node in the tree, we need to check for all possible cuts.
+		 * Let q be the length of a cut (hence, q < k), then we are in the following situation:
+		 * 
+		 * S1 [   x1    |         x2         ]
+		 *    i         i + q                i + k - 1
+		 * 
+		 * here we have two possibilities:
+		 *      
+		 * S2 [   y1    |         y2         ]
+		 *    j         j + q                j + k - 1
+		 *    
+		 * or 
+		 * 
+		 * S2 [       y1        |     y2     ]
+		 *    j                 j + k - q    j + k - 1
+		 * 
+		 * which in terms of F means:
+		 * 
+		 * F(i, j, k) = for some 1 <= q < k we have:
+		 *  (F(i, j, q) AND F(i + q, j + q, k - q)) OR (F(i, j + k - q, q) AND F(i + q, j, k - q))
+		 *  
+		 * Base case is k = 1, where we simply need to check for S1[i] and S2[j] to be equal 
+		 * */
+		boolean [][][] F = new boolean[len][len][len + 1];
+		for (int k = 1; k <= len; ++k)
+			for (int i = 0; i + k <= len; ++i)
+				for (int j = 0; j + k <= len; ++j)
+					if (k == 1)
+						F[i][j][k] = s1.charAt(i) == s2.charAt(j);
+					else for (int q = 1; q < k && !F[i][j][k]; ++q) {
+						F[i][j][k] = (F[i][j][q] && F[i + q][j + q][k - q]) || (F[i][j + k - q][q] && F[i + q][j][k - q]);
+					}
+		return F[0][0][len];
+	}
+}
+"""
