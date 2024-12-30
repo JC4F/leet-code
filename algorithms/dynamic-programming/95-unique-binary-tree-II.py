872f39f215a00d6692b35fb4fcbc51ab9de77595
JC4F <lequan12122002@gmail.com>
2024-12-30 22:14:12 +0700

    95-unique-binary-tree-dp

--- algorithms/dynamic-programming/95-unique-binary-tree-II.py
+++ algorithms/dynamic-programming/95-unique-binary-tree-II.py
@@ -37,3 +37,75 @@ private List<TreeNode> generateSubtrees(int s, int e) {
 	return res;
 }
 """
+
+"""
+# dp solution
+# ===> ideal same as summarize in 96
+******
+for clone ===> This is because for the left nodes, they always start from 1, which have the same values with those in dp[]; 
+While the right nodes, starting at j+1, so they need offset = j+1;
+
+public static List<TreeNode> generateTrees(int n) {
+    List<TreeNode>[] result = new List[n + 1];
+    result[0] = new ArrayList<TreeNode>();
+    if (n == 0) {
+        return result[0];
+    }
+
+    result[0].add(null);
+    for (int len = 1; len <= n; len++) {
+        result[len] = new ArrayList<TreeNode>();
+        for (int j = 0; j < len; j++) {
+            for (TreeNode nodeL : result[j]) {
+                for (TreeNode nodeR : result[len - j - 1]) {
+                    TreeNode node = new TreeNode(j + 1);
+                    node.left = nodeL;
+                    node.right = clone(nodeR, j + 1);
+                    result[len].add(node);
+                }
+            }
+        }
+    }
+    return result[n];
+}
+
+private static TreeNode clone(TreeNode n, int offset) {
+    if (n == null) {
+        return null;
+    }
+    TreeNode node = new TreeNode(n.val + offset);
+    node.left = clone(n.left, offset);
+    node.right = clone(n.right, offset);
+    return node;
+}
+"""
+
+"""
+same like method 1 but have memo
+class Solution:
+    def allPossibleBST(self, start, end, memo):
+        res = []
+        if start > end:
+            res.append(None)
+            return res
+        if (start, end) in memo:
+            return memo[(start, end)]
+
+        # Iterate through all values from start to end to construct left and right subtree recursively.
+        for i in range(start, end + 1):
+            leftSubTrees = self.allPossibleBST(start, i - 1, memo)
+            rightSubTrees = self.allPossibleBST(i + 1, end, memo)
+
+            # Loop through all left and right subtrees and connect them to ith root.
+            for left in leftSubTrees:
+                for right in rightSubTrees:
+                    root = TreeNode(i, left, right)
+                    res.append(root)
+
+        memo[(start, end)] = res
+        return res
+
+    def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
+        memo = {}
+        return self.allPossibleBST(1, n, memo)
+"""
