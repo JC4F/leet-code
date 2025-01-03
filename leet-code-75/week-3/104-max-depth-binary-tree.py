695475630f0314bf4256a4fa37826711e34592c8
JC4F <lequan12122002@gmail.com>
2025-01-03 23:52:47 +0700

    104-max-depth-binary-tree-bfs

--- leet-code-75/week-3/104-max-depth-binary-tree.py
+++ leet-code-75/week-3/104-max-depth-binary-tree.py
@@ -21,3 +21,34 @@ class Solution:
             return max(dfs(root.left, depth + 1), dfs(root.right, depth + 1))
 
         return dfs(root, 0)
+
+
+"""
+BFS:
+
+int maxDepth(TreeNode *root)
+{
+    if(root == NULL)
+        return 0;
+    
+    int res = 0;
+    queue<TreeNode *> q;
+    q.push(root);
+    while(!q.empty())
+    {
+        ++ res;
+        for(int i = 0, n = q.size(); i < n; ++ i)
+        {
+            TreeNode *p = q.front();
+            q.pop();
+            
+            if(p -> left != NULL)
+                q.push(p -> left);
+            if(p -> right != NULL)
+                q.push(p -> right);
+        }
+    }
+    
+    return res;
+}
+"""
