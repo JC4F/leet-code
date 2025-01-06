ab58ccdb0809b6fdcf974024b9f0c48614e01dd7
JC4F <lequan12122002@gmail.com>
2025-01-06 22:26:47 +0700

    108-convert-sorted-array-to-binary-search-tree

---
+++ algorithms/binary-tree/108-convert-sorted-array-to-binary-search-tree.py
@@ -0,0 +1,21 @@
+"""
+public TreeNode sortedArrayToBST(int[] num) {
+    if (num.length == 0) {
+        return null;
+    }
+    TreeNode head = helper(num, 0, num.length - 1);
+    return head;
+}
+
+public TreeNode helper(int[] num, int low, int high) {
+    if (low > high) { // Done
+        return null;
+    }
+    int mid = (low + high) / 2;  ====> int mid = low + (high-low)/2; // avoids integer overflow
+    TreeNode node = new TreeNode(num[mid]);
+    node.left = helper(num, low, mid - 1);
+    node.right = helper(num, mid + 1, high);
+    return node;
+}
+
+"""
