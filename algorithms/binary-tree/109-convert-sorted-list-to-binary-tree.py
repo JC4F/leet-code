7aad3c1cdeee97e514640c87fe13865bdbd5e074
JC4F <lequan12122002@gmail.com>
2025-01-07 20:44:42 +0700

    109-convert-sorted-list-to-binary-tree

---
+++ algorithms/binary-tree/109-convert-sorted-list-to-binary-tree.py
@@ -0,0 +1,43 @@
+"""
+public class Solution {
+public TreeNode sortedListToBST(ListNode head) {
+    if(head==null) return null;
+    return toBST(head,null);
+}
+public TreeNode toBST(ListNode head, ListNode tail){
+    ListNode slow = head;
+    ListNode fast = head;
+    if(head==tail) return null;
+
+    while(fast!=tail&&fast.next!=tail){
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    TreeNode thead = new TreeNode(slow.val);
+    thead.left = toBST(head,slow);
+    thead.right = toBST(slow.next,tail);
+    return thead;
+}
+"""
+
+# ===>
+"""
+Personally think that using a head and a tail (exclusive) is not that easy to memorize.
+I found that we can simply cut the left sub list and then recur, so that the recursion stops itself.
+
+    public TreeNode sortedListToBST(ListNode head) {
+        if (head == null) return null;
+        if (head.next == null) return new TreeNode(head.val);
+        ListNode slow = head, pre = null, fast = head;
+        while (fast != null && fast.next != null) {
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        pre.next = null; //cut left sub list here
+        TreeNode n = new TreeNode(slow.val);
+        n.left = sortedListToBST(head);
+        n.right = sortedListToBST(slow.next);
+        return n;
+    }
+"""
