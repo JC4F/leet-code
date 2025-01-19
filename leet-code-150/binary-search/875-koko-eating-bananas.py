3a496b8dfdf727cb2d2c43fc86fa567134f33cc4
JC4F <lequan12122002@gmail.com>
2025-01-19 21:11:01 +0700

    875-koko-eating-bananas

---
+++ leet-code-150/binary-search/875-koko-eating-bananas.py
@@ -0,0 +1,25 @@
+"""
+Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
+Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
+Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
+Return the minimum integer k such that she can eat all the bananas within h hours.
+Example 1:
+
+Input: piles = [3,6,7,11], h = 8
+Output: 4
+"""
+
+
+def minEatingSpeed(piles: List[int], H: int) -> int:
+    def feasible(speed) -> bool:
+        # return sum(math.ceil(pile / speed) for pile in piles) <= H  # slower
+        return sum((pile - 1) / speed + 1 for pile in piles) <= H  # faster
+
+    left, right = 1, max(piles)
+    while left < right:
+        mid = left + (right - left) // 2
+        if feasible(mid):
+            right = mid
+        else:
+            left = mid + 1
+    return left
