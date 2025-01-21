05a6a9a1db59fb1f44590998f43605c4c54354ba
JC4F <lequan12122002@gmail.com>
2025-01-21 23:41:46 +0700

    4-median-of-two-soft-array

---
+++ leet-code-150/binary-search/4-median-of-two-soft-array.py
@@ -0,0 +1,38 @@
+"""
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+The overall run time complexity should be O(log (m+n)).
+
+
+
+Example 1:
+
+Input: nums1 = [1,3], nums2 = [2]
+Output: 2.00000
+Explanation: merged array = [1,2,3] and median is 2.
+Example 2:
+
+Input: nums1 = [1,2], nums2 = [3,4]
+Output: 2.50000
+Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+"""
+
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        len1 = len(nums1)
+        len2 = len(nums2)
+        merged = nums1 + nums2
+        merged.sort()
+
+        totalLen = len(merged)
+        if totalLen % 2 == 0:
+            return (merged[totalLen // 2 - 1] + merged[totalLen // 2]) / 2.0
+        else:
+            return merged[totalLen // 2]
+
+
+"""
+Time: O((n+m)log(n+m))
+Space: O(n+m)
+"""
