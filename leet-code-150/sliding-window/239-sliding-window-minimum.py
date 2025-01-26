64c6afc1826b322cbe074e5175eeb1e90b923144
JC4F <lequan12122002@gmail.com>
2025-01-26 23:25:56 +0700

    239-sliding-window-minimum

---
+++ leet-code-150/sliding-window/239-sliding-window-minimum.py
@@ -0,0 +1,120 @@
+"""
+You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+
+Return the max sliding window.
+
+
+
+Example 1:
+
+Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+Output: [3,3,5,5,6,7]
+Explanation:
+Window position                Max
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+"""
+
+"""
+1. Brute force
+Time complexity: O(n∗k)
+Space complexity: O(1)
+"""
+
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        output = []
+
+        for i in range(len(nums) - k + 1):
+            maxi = nums[i]
+            for j in range(i, i + k):
+                maxi = max(maxi, nums[j])
+            output.append(maxi)
+
+        return output
+
+
+"""
+2. Segment tree
+Time complexity: O(nlogn)
+Space complexity: O(n)
+"""
+
+
+class SegmentTree:
+    def __init__(self, N, A):
+        self.n = N
+        while (self.n & (self.n - 1)) != 0:
+            self.n += 1
+        self.build(N, A)
+
+    def build(self, N, A):
+        self.tree = [float("-inf")] * (2 * self.n)
+        for i in range(N):
+            self.tree[self.n + i] = A[i]
+        for i in range(self.n - 1, 0, -1):
+            self.tree[i] = max(self.tree[i << 1], self.tree[i << 1 | 1])
+
+    def query(self, l, r):
+        res = float("-inf")
+        l += self.n
+        r += self.n + 1
+        while l < r:
+            if l & 1:
+                res = max(res, self.tree[l])
+                l += 1
+            if r & 1:
+                r -= 1
+                res = max(res, self.tree[r])
+            l >>= 1
+            r >>= 1
+        return res
+
+
+class Solution:
+    def maxSlidingWindow(self, nums, k):
+        n = len(nums)
+        segTree = SegmentTree(n, nums)
+        output = []
+        for i in range(n - k + 1):
+            output.append(segTree.query(i, i + k - 1))
+        return output
+
+
+"""
+3, 4
+"""
+
+"""
+5. Deque monotonically decreasing
+Time complexity: O(n)
+Space complexity: O(n)
+"""
+
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        output = []
+        q = deque()  # index
+        l = r = 0
+
+        while r < len(nums):
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+            q.append(r)
+
+            if l > q[0]:
+                q.popleft()
+
+            if (r + 1) >= k:
+                output.append(nums[q[0]])
+                l += 1
+            r += 1
+
+        return output
