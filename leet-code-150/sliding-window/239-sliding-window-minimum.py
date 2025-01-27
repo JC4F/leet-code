542359720587063527cc0208b9bed291a7004c51
JC4F <lequan12122002@gmail.com>
2025-01-27 22:59:53 +0700

    239-sliding-window-minimum heap, dp

--- leet-code-150/sliding-window/239-sliding-window-minimum.py
+++ leet-code-150/sliding-window/239-sliding-window-minimum.py
@@ -88,8 +88,62 @@ class Solution:
 
 
 """
-3, 4
+3. Heap
+Time complexity: O(nlogn)
+Space complexity: O(n)
 """
+
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        heap = []
+        output = []
+        for i in range(len(nums)):
+            heapq.heappush(heap, (-nums[i], i))
+            if i >= k - 1:
+                while heap[0][1] <= i - k:
+                    heapq.heappop(heap)
+                output.append(-heap[0][0])
+        return output
+
+
+"""
+4. Dynamic programming
+Time complexity: O(n)
+Space complexity: O(n)
+
+https://leetcode.com/problems/sliding-window-maximum/solutions/458121/java-all-solutions-b-f-pq-deque-dp-with-explanation-and-complexity-analysis/
+nhìn ảnh trong hình để cảm nhận rõ hơn
+"""
+
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        n = len(nums)
+        leftMax = [0] * n
+        rightMax = [0] * n
+
+        leftMax[0] = nums[0]
+        rightMax[n - 1] = nums[n - 1]
+
+        for i in range(1, n):
+            if i % k == 0:
+                leftMax[i] = nums[i]
+            else:
+                leftMax[i] = max(leftMax[i - 1], nums[i])
+
+            if (n - 1 - i) % k == 0:
+                rightMax[n - 1 - i] = nums[n - 1 - i]
+            else:
+                rightMax[n - 1 - i] = max(rightMax[n - i], nums[n - 1 - i])
+
+        output = [0] * (n - k + 1)
+
+        for i in range(n - k + 1):
+            output[i] = max(leftMax[i + k - 1], rightMax[i])
+
+        return output
+
 
 """
 5. Deque monotonically decreasing
