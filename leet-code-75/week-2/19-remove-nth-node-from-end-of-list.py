7b34ba901de81990ccc472a3c480d8021066561c
JC4F <lequan12122002@gmail.com>
2025-01-31 23:15:44 +0700

    refactor-19-remove-nth-node-from-edn

--- leet-code-75/week-2/19-remove-nth-node-from-end-of-list.py
+++ leet-code-75/week-2/19-remove-nth-node-from-end-of-list.py
@@ -1,4 +1,4 @@
-'''
+"""
 Example 1:
 Input: head = [1,2,3,4,5], n = 2
 Output: [1,2,3,5]
@@ -10,22 +10,50 @@ Example 3:
 
 Input: head = [1,2], n = 1
 Output: [1]
+"""
+
+"""
+1. Recursion
+Time complexity: O(n)
+Space complexity: O(n)
+"""
 
 
-====>
-
-A one pass solution can be done using pointers. Move one pointer fast --> n+1 places forward, to maintain a gap of n between the two pointers and then move both at the same speed. Finally, when the fast pointer reaches the end, the slow pointer will be n+1 places behind - just the right spot for it to be able to skip the next node.
-
-Since the question gives that n is valid, not too many checks have to be put in place. Otherwise, this would be necessary.
-
-
-solution 1: 
 class Solution:
-    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
-        fast, slow = head, head
-        for _ in range(n): fast = fast.next
-        if not fast: return head.next
-        while fast.next: fast, slow = fast.next, slow.next
-        slow.next = slow.next.next
+    def rec(self, head, n):
+        if not head:
+            return None
+
+        head.next = self.rec(head.next, n)
+        n[0] -= 1
+        if n[0] == 0:
+            return head.next
         return head
-'''
+
+    def removeNthFromEnd(self, head, n):
+        return self.rec(head, [n])
+
+
+"""
+2. Two pointers
+Time complexity: O(n)
+Space complexity: O(1)
+"""
+
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        left = dummy
+        right = head
+
+        while n > 0:
+            right = right.next
+            n -= 1
+
+        while right:
+            left = left.next
+            right = right.next
+
+        left.next = left.next.next
+        return dummy.nexts
