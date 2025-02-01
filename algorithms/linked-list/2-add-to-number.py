06d1bf089ea289c11943e74fe3ba19015a7cc87a
JC4F <lequan12122002@gmail.com>
2025-02-02 00:06:25 +0700

    2-add-sum update linked list

--- algorithms/linked-list/2-add-to-number.py
+++ algorithms/linked-list/2-add-to-number.py
@@ -4,28 +4,62 @@ Output: [7,0,8]
 Explanation: 342 + 465 = 807.
 """
 
+"""
+1. Recursion
+Time complexity: O(m+n)
+Space complexity: O(m+n)
+"""
+
 
 class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        dummyHead = ListNode(0)
-        tail = dummyHead
+    def add(
+        self, l1: Optional[ListNode], l2: Optional[ListNode], carry: int
+    ) -> Optional[ListNode]:
+        if not l1 and not l2 and carry == 0:
+            return None
+
+        v1 = l1.val if l1 else 0
+        v2 = l2.val if l2 else 0
+
+        carry, val = divmod(v1 + v2 + carry, 10)
+
+        next_node = self.add(l1.next if l1 else None, l2.next if l2 else None, carry)
+        return ListNode(val, next_node)
+
+    def addTwoNumbers(
+        self, l1: Optional[ListNode], l2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        return self.add(l1, l2, 0)
+
+
+"""
+2. Iteration
+Time complexity: O(m+n)
+Space complexity: O(1)
+"""
+
+
+class Solution:
+    def addTwoNumbers(
+        self, l1: Optional[ListNode], l2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        dummy = ListNode()
+        cur = dummy
+
         carry = 0
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
 
-        while l1 is not None or l2 is not None or carry != 0:
-            digit1 = l1.val if l1 is not None else 0
-            digit2 = l2.val if l2 is not None else 0
+            # new digit
+            val = v1 + v2 + carry
+            carry = val // 10
+            val = val % 10
+            cur.next = ListNode(val)
 
-            sum = digit1 + digit2 + carry
-            digit = sum % 10
-            carry = sum // 10
+            # update ptrs
+            cur = cur.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
 
-            newNode = ListNode(digit)
-            tail.next = newNode
-            tail = tail.next
-
-            l1 = l1.next if l1 is not None else None
-            l2 = l2.next if l2 is not None else None
-
-        result = dummyHead.next
-        dummyHead.next = None
-        return result
+        return dummy.next
