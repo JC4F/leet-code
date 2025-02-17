9991993f5163d79dde3bf76701d3e673af3dded3
jc4f <lequan12122002@gmail.com>
2025-02-17 16:59:05 +0700

    199-binary-tree-right-side-view

---
+++ leet-code-150/tree/199-binary-tree-right-side-view.py
@@ -0,0 +1,49 @@
+"""
+1. DFS
+Time complexity: O(n)
+Space complexity: O(n)
+"""
+
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+
+        def dfs(node, depth):
+            if not node:
+                return None
+            if depth == len(res):
+                res.append(node.val)
+
+            dfs(node.right, depth + 1)
+            dfs(node.left, depth + 1)
+
+        dfs(root, 0)
+        return res
+
+
+"""
+2. BFS
+Time complexity: O(n)
+Space complexity: O(n)
+"""
+
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        q = deque([root])
+
+        while q:
+            rightSide = None
+            qLen = len(q)
+
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    rightSide = node
+                    q.append(node.left)
+                    q.append(node.right)
+            if rightSide:
+                res.append(rightSide.val)
+        return res
