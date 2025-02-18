0690afda40a1ad128c1646666c4b19c4019905b2
jc4f <lequan12122002@gmail.com>
2025-02-18 11:03:40 +0700

    update-230-kth-smallest-using-morris

--- leet-code-75/week-4/230-kth-smallest-ele-in-bst.py
+++ leet-code-75/week-4/230-kth-smallest-ele-in-bst.py
@@ -35,3 +35,38 @@ class Solution:
             if k == 0:
                 return curr.val
             curr = curr.right
+
+
+"""
+3. Morris traversal
+Time complexity: O(n)
+Space complexity: O(1)
+"""
+
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        curr = root
+
+        while curr:
+            if not curr.left:
+                k -= 1
+                if k == 0:
+                    return curr.val
+                curr = curr.right
+            else:
+                pred = curr.left
+                while pred.right and pred.right != curr:
+                    pred = pred.right
+
+                if not pred.right:
+                    pred.right = curr
+                    curr = curr.left
+                else:
+                    pred.right = None
+                    k -= 1
+                    if k == 0:
+                        return curr.val
+                    curr = curr.right
+
+        return -1
