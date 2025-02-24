2e4ba0ec33dc4e65d869cb9c3cf9ee7ae42db542
jc4f <lequan12122002@gmail.com>
2025-02-24 08:49:25 +0700

    update 200-number-of-island-graph

--- leet-code-75/week-2/200-number-of-island.py
+++ leet-code-75/week-2/200-number-of-island.py
@@ -1,9 +1,9 @@
-'''
+"""
 Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
 
 An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
 
- 
+
 
 Example 1:
 
@@ -23,36 +23,75 @@ Input: grid = [
   ["0","0","0","1","1"]
 ]
 Output: 3
+"""
 
-=====> 
+"""
+1. DFS
+Time complexity: O(m*n)
+Space complexity: O(m*n)
+"""
 
-Iterate through each of the cell and if it is an island, do dfs to mark all adjacent islands, then increase the counter by 1.
 
-def numIslands(self, grid):
-    if not grid:
-        return 0
-        
-    count = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[i][j] == '1':
-                self.dfs(grid, i, j)
-                count += 1
-    return count
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        ROWS, COLS = len(grid), len(grid[0])
+        islands = 0
 
-def dfs(self, grid, i, j):
-    if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] != '1':
-        return
-    grid[i][j] = '#'
-    self.dfs(grid, i+1, j)
-    self.dfs(grid, i-1, j)
-    self.dfs(grid, i, j+1)
-    self.dfs(grid, i, j-1)
-'''
+        def dfs(r, c):
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS or grid[r][c] == "0":
+                return
 
-'''
-Depth first search
+            grid[r][c] = "0"
+            for dr, dc in directions:
+                dfs(r + dr, c + dc)
 
-https://www.geeksforgeeks.org/depth-first-search-or-dfs-for-a-graph/
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1":
+                    dfs(r, c)
+                    islands += 1
 
-'''
+        return islands
+
+
+"""
+2. BFS
+Time complexity: O(m*n)
+Space complexity: O(m*n)
+"""
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        ROWS, COLS = len(grid), len(grid[0])
+        islands = 0
+
+        def bfs(r, c):
+            q = deque()
+            grid[r][c] = "0"
+            q.append((r, c))
+
+            while q:
+                row, col = q.popleft()
+                for dr, dc in directions:
+                    nr, nc = dr + row, dc + col
+                    if (
+                        nr < 0
+                        or nc < 0
+                        or nr >= ROWS
+                        or nc >= COLS
+                        or grid[nr][nc] == "0"
+                    ):
+                        continue
+                    q.append((nr, nc))
+                    grid[nr][nc] = "0"
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1":
+                    bfs(r, c)
+                    islands += 1
+
+        return islands
