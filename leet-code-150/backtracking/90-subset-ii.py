8ab12308bab672555624e10a7d858d41b9f9c00b
jc4f <lequan12122002@gmail.com>
2025-03-03 08:46:01 +0700

    90-subset-ii

---
+++ leet-code-150/backtracking/90-subset-ii.py
@@ -0,0 +1,65 @@
+"""
+You are given an array nums of integers, which may contain duplicates. Return all possible subsets.
+
+The solution must not contain duplicate subsets. You may return the solution in any order.
+
+Example 1:
+
+Input: nums = [1,2,1]
+
+Output: [[],[1],[1,2],[1,1],[1,2,1],[2]]
+"""
+
+"""
+1. Backtracking (Pick / Not Pick)
+Time complexity: O(n * 2^n)
+Space complexity: O(n)
+"""
+
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+
+        def backtrack(i, subset):
+            if i == len(nums):
+                res.append(subset[::])
+                return
+
+            subset.append(nums[i])
+            backtrack(i + 1, subset)
+            subset.pop()
+
+            while i + 1 < len(nums) and nums[i] == nums[i + 1]:
+                i += 1
+            backtrack(i + 1, subset)
+
+        backtrack(0, [])
+        return res
+
+
+"""
+2. Backtracking (Iteration)
+Time complexity: O(n * 2^n)
+Space complexity: O(n)
+"""
+
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+
+        def backtrack(i, subset):
+            res.append(subset[::])
+
+            for j in range(i, len(nums)):
+                if j > i and nums[j] == nums[j - 1]:
+                    continue
+                subset.append(nums[j])
+                backtrack(j + 1, subset)
+                subset.pop()
+
+        backtrack(0, [])
+        return res
