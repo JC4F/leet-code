94b2190cc7ee3c18d578756d0500f80161470042
jc4f <lequan12122002@gmail.com>
2025-03-04 09:54:17 +0700

    131-palindrome-partitioning bt dp

--- leet-code-150/backtracking/131-palindrome-partitioning.py
+++ leet-code-150/backtracking/131-palindrome-partitioning.py
@@ -75,3 +75,36 @@ class Solution:
                 return False
             l, r = l + 1, r - 1
         return True
+
+
+"""
+3. Backtracking (DP)
+Time complexity: O(n * 2^n)
+Space complexity: O(n^2)
+"""
+
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        for l in range(1, n + 1):
+            for i in range(n - l + 1):
+                dp[i][i + l - 1] = s[i] == s[i + l - 1] and (
+                    i + 1 > (i + l - 2) or dp[i + 1][i + l - 2]
+                )
+
+        res, part = [], []
+
+        def dfs(i):
+            if i >= len(s):
+                res.append(part.copy())
+                return
+            for j in range(i, len(s)):
+                if dp[i][j]:
+                    part.append(s[i : j + 1])
+                    dfs(j + 1)
+                    part.pop()
+
+        dfs(0)
+        return res
