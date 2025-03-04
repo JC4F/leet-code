89cbdd42897372f20879b828851abc1e3bc40b5e
jc4f <lequan12122002@gmail.com>
2025-03-04 09:14:18 +0700

    131-palindrome-partitioning

---
+++ leet-code-150/backtracking/131-palindrome-partitioning.py
@@ -0,0 +1,77 @@
+"""
+Given a string s, split s into substrings where every substring is a palindrome. Return all possible lists of palindromic substrings.
+
+You may return the solution in any order.
+
+Example 1:
+
+Input: s = "aab"
+
+Output: [["a","a","b"],["aa","b"]]
+"""
+
+"""
+1. Backtracking (Pick/Not Pick)
+Time complexity: O(n * 2^n)
+Space complexity: O(n)
+"""
+
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        res, part = [], []
+
+        def dfs(j, i):
+            if i >= len(s):
+                if i == j:
+                    res.append(part.copy())
+                return
+
+            if self.isPali(s, j, i):
+                part.append(s[j : i + 1])
+                dfs(i + 1, i + 1)
+                part.pop()
+
+            dfs(j, i + 1)
+
+        dfs(0, 0)
+        return res
+
+    def isPali(self, s, l, r):
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            l, r = l + 1, r - 1
+        return True
+
+
+"""
+2. Backtracking (Iteration)
+Time complexity: O(n * 2^n)
+Space complexity: O(n)
+"""
+
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        res, part = [], []
+
+        def dfs(i):
+            if i >= len(s):
+                res.append(part.copy())
+                return
+            for j in range(i, len(s)):
+                if self.isPali(s, i, j):
+                    part.append(s[i : j + 1])
+                    dfs(j + 1)
+                    part.pop()
+
+        dfs(0)
+        return res
+
+    def isPali(self, s, l, r):
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            l, r = l + 1, r - 1
+        return True
