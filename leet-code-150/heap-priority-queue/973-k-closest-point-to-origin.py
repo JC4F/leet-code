09ba8f5fb7c23b3b6d3bec47cd365af884d2797f
JC4F <lequan12122002@gmail.com>
2025-03-09 18:47:42 +0700

    973-k-closest-point-to-origin

---
+++ leet-code-150/heap-priority-queue/973-k-closest-point-to-origin.py
@@ -0,0 +1,45 @@
+"""
+You are given an 2-D array points where points[i] = [xi, yi] represents the coordinates of a point on an X-Y axis plane. You are also given an integer k.
+
+Return the k closest points to the origin (0, 0).
+
+The distance between two points is defined as the Euclidean distance (sqrt((x1 - x2)^2 + (y1 - y2)^2)).
+
+You may return the answer in any order.
+Input: points = [[0,2],[2,2]], k = 1
+
+Output: [[0,2]]
+Explanation : The distance between (0, 2) and the origin (0, 0) is 2. The distance between (2, 2) and the origin is sqrt(2^2 + 2^2) = 2.82842. So the closest point to the origin is (0, 2).
+"""
+
+"""
+1. Sorting
+"""
+
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        points.sort(key=lambda p: p[0] ** 2 + p[1] ** 2)
+        return points[:k]
+
+
+"""
+2. Min Heap
+"""
+
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        minHeap = []
+        for x, y in points:
+            dist = (x**2) + (y**2)
+            minHeap.append([dist, x, y])
+
+        heapq.heapify(minHeap)
+        res = []
+        while k > 0:
+            dist, x, y = heapq.heappop(minHeap)
+            res.append([x, y])
+            k -= 1
+
+        return res
