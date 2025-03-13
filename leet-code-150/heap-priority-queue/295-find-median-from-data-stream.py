bf76ce3f438d81e2673f2d09a3eb0846a056a8f6
jc4f <lequan12122002@gmail.com>
2025-03-13 08:59:03 +0700

    295-find-median-from-data-stream

---
+++ leet-code-150/heap-priority-queue/295-find-median-from-data-stream.py
@@ -0,0 +1,91 @@
+"""
+The median is the middle value in a sorted list of integers. For lists of even length, there is no middle value, so the median is the mean of the two middle values.
+
+For example:
+
+For arr = [1,2,3], the median is 2.
+For arr = [1,2], the median is (1 + 2) / 2 = 1.5
+Implement the MedianFinder class:
+
+MedianFinder() initializes the MedianFinder object.
+void addNum(int num) adds the integer num from the data stream to the data structure.
+double findMedian() returns the median of all elements so far.
+Example 1:
+
+Input:
+["MedianFinder", "addNum", "1", "findMedian", "addNum", "3" "findMedian", "addNum", "2", "findMedian"]
+
+Output:
+[null, null, 1.0, null, 2.0, null, 2.0]
+
+Explanation:
+MedianFinder medianFinder = new MedianFinder();
+medianFinder.addNum(1);    // arr = [1]
+medianFinder.findMedian(); // return 1.0
+medianFinder.addNum(3);    // arr = [1, 3]
+medianFinder.findMedian(); // return 2.0
+medianFinder.addNum(2);    // arr[1, 2, 3]
+medianFinder.findMedian(); // return 2.0
+"""
+
+"""
+1. Sorting
+Time complexity: O(m) for addNum(), O(m*nlogn) for findMedian()
+Space complexity: O(n)
+m: number of function calls and n: length of the array
+"""
+
+
+import heapq
+
+
+class MedianFinder:
+    def __init__(self):
+        self.data = []
+
+    def addNum(self, num: int) -> None:
+        self.data.append(num)
+
+    def findMedian(self) -> float:
+        self.data.sort()
+        n = len(self.data)
+        return (
+            self.data[n // 2]
+            if (n & 1)
+            else (self.data[n // 2] + self.data[n // 2 - 1]) / 2
+        )
+
+
+"""
+2. Heap
+Time complexity: O(m * logn) for addNum(), O(m) for findMedian()
+Space complexity: O(n)
+m: number of function calls and n: length of the array
+"""
+
+
+class MedianFinder:
+    def __init__(self):
+        # two heaps, large, small, minheap, maxheap
+        # heaps should be equal size
+        self.small, self.large = [], []
+
+    def addNum(self, num: int) -> None:
+        if self.large and num > self.large[0]:
+            heapq.heappush(self.large, num)
+        else:
+            heapq.heappush(self.small, -1 * num)
+
+        if len(self.small) > len(self.large) + 1:
+            val = -1 * heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+        if len(self.large) > len(self.small) + 1:
+            val = heapq.heappop(self.large)
+            heapq.heappush(self.small, -1 * val)
+
+    def findMedian(self) -> float:
+        if len(self.small) > len(self.large):
+            return -1 * self.small[0]
+        elif len(self.large) > len(self.small):
+            return self.large[0]
+        return (-1 * self.small[0] + self.large[0]) / 2.0
