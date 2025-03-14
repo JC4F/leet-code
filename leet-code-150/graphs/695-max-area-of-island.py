e485d99bba5d91946ab70447838cade98e3e13ed
jc4f <lequan12122002@gmail.com>
2025-03-14 09:00:49 +0700

    695-max-area-of-island

---
+++ leet-code-150/graphs/695-max-area-of-island.py
@@ -0,0 +1,95 @@
+"""
+You are given a matrix grid where grid[i] is either a 0 (representing water) or 1 (representing land).
+
+An island is defined as a group of 1's connected horizontally or vertically. You may assume all four edges of the grid are surrounded by water.
+
+The area of an island is defined as the number of cells within the island.
+
+Return the maximum area of an island in grid. If no island exists, return 0.
+
+Example 1:
+Input: grid = [
+  [0,1,1,0,1],
+  [1,0,1,0,1],
+  [0,1,1,0,1],
+  [0,1,0,0,1]
+]
+
+Output: 6
+Explanation: 1's cannot be connected diagonally, so the maximum area of the island is 6.
+"""
+
+"""
+1. DFS
+Time complexity: O(m*n)
+Space complexity: O(m*n)
+"""
+
+
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        visit = set()
+
+        def dfs(r, c):
+            if (
+                r < 0
+                or r == ROWS
+                or c < 0
+                or c == COLS
+                or grid[r][c] == 0
+                or (r, c) in visit
+            ):
+                return 0
+            visit.add((r, c))
+            return 1 + dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c + 1) + dfs(r, c - 1)
+
+        area = 0
+        for r in range(ROWS):
+            for c in range(COLS):
+                area = max(area, dfs(r, c))
+        return area
+
+
+"""
+2. BFS
+Time complexity: O(m*n)
+Space complexity: O(m*n)
+"""
+
+
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        ROWS, COLS = len(grid), len(grid[0])
+        area = 0
+
+        def bfs(r, c):
+            q = deque()
+            grid[r][c] = 0
+            q.append((r, c))
+            res = 1
+
+            while q:
+                row, col = q.popleft()
+                for dr, dc in directions:
+                    nr, nc = dr + row, dc + col
+                    if (
+                        nr < 0
+                        or nc < 0
+                        or nr >= ROWS
+                        or nc >= COLS
+                        or grid[nr][nc] == 0
+                    ):
+                        continue
+                    q.append((nr, nc))
+                    grid[nr][nc] = 0
+                    res += 1
+            return res
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1:
+                    area = max(area, bfs(r, c))
+
+        return area
