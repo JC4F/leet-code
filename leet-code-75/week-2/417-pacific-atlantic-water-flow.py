e5caf27167835fc1ea32fe677453886cc5fe1e09
jc4f <lequan12122002@gmail.com>
2025-03-15 11:40:04 +0700

    417-pacific-atlantic-water-update

--- leet-code-75/week-2/417-pacific-atlantic-water-flow.py
+++ leet-code-75/week-2/417-pacific-atlantic-water-flow.py
@@ -1,94 +1,156 @@
 """
-Two Queue and add all the Pacific border to one queue; Atlantic border to another queue.
-Keep a visited matrix for each queue. In the end, add the cell visited by two queue to the result.
-BFS: Water flood from ocean to the cell. Since water can only flow from high/equal cell to low cell, add the neighboor cell with height larger or equal to current cell to the queue and mark as visited.
-public class Solution {
-    int[][]dir = new int[][]{{1,0},{-1,0},{0,1},{0,-1}}; => 4 huong
-    public List<int[]> pacificAtlantic(int[][] matrix) {
-        List<int[]> res = new LinkedList<>();
-        if(matrix == null || matrix.length == 0 || matrix[0].length == 0){
-            return res;
-        }
-        int n = matrix.length, m = matrix[0].length;
-        //One visited map for each ocean
-        boolean[][] pacific = new boolean[n][m];
-        boolean[][] atlantic = new boolean[n][m];
-        Queue<int[]> pQueue = new LinkedList<>();
-        Queue<int[]> aQueue = new LinkedList<>();
-        for(int i=0; i<n; i++){ //Vertical border
-            pQueue.offer(new int[]{i, 0});
-            aQueue.offer(new int[]{i, m-1});
-            pacific[i][0] = true;
-            atlantic[i][m-1] = true;
-        }
-        for(int i=0; i<m; i++){ //Horizontal border
-            pQueue.offer(new int[]{0, i});
-            aQueue.offer(new int[]{n-1, i});
-            pacific[0][i] = true;
-            atlantic[n-1][i] = true;
-        }
-        bfs(matrix, pQueue, pacific);
-        bfs(matrix, aQueue, atlantic);
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(pacific[i][j] && atlantic[i][j])
-                    res.add(new int[]{i,j});
-            }
-        }
-        return res;
-    }
-    public void bfs(int[][]matrix, Queue<int[]> queue, boolean[][]visited){
-        int n = matrix.length, m = matrix[0].length;
-        while(!queue.isEmpty()){
-            int[] cur = queue.poll();
-            for(int[] d:dir){
-                int x = cur[0]+d[0];
-                int y = cur[1]+d[1];
-                if(x<0 || x>=n || y<0 || y>=m || visited[x][y] || matrix[x][y] < matrix[cur[0]][cur[1]]){
-                    continue;
-                }
-                visited[x][y] = true;
-                queue.offer(new int[]{x, y});
-            }
-        }
-    }
-}
-DFS version:
+You are given a rectangular island heights where heights[r][c] represents the height above sea level of the cell at coordinate (r, c).
 
-public class Solution {
-    public List<int[]> pacificAtlantic(int[][] matrix) {
-        List<int[]> res = new LinkedList<>();
-        if(matrix == null || matrix.length == 0 || matrix[0].length == 0){
-            return res;
-        }
-        int n = matrix.length, m = matrix[0].length;
-        boolean[][]pacific = new boolean[n][m];
-        boolean[][]atlantic = new boolean[n][m];
-        for(int i=0; i<n; i++){
-            dfs(matrix, pacific, Integer.MIN_VALUE, i, 0);
-            dfs(matrix, atlantic, Integer.MIN_VALUE, i, m-1);
-        }
-        for(int i=0; i<m; i++){
-            dfs(matrix, pacific, Integer.MIN_VALUE, 0, i);
-            dfs(matrix, atlantic, Integer.MIN_VALUE, n-1, i);
-        }
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < m; j++)
-                if (pacific[i][j] && atlantic[i][j])
-                    res.add(new int[] {i, j});
-        return res;
-    }
+The islands borders the Pacific Ocean from the top and left sides, and borders the Atlantic Ocean from the bottom and right sides.
 
-    int[][]dir = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
+Water can flow in four directions (up, down, left, or right) from a cell to a neighboring cell with height equal or lower. Water can also flow into the ocean from cells adjacent to the ocean.
 
-    public void dfs(int[][]matrix, boolean[][]visited, int height, int x, int y){
-        int n = matrix.length, m = matrix[0].length;
-        if(x<0 || x>=n || y<0 || y>=m || visited[x][y] || matrix[x][y] < height)
-            return;
-        visited[x][y] = true;
-        for(int[]d:dir){
-            dfs(matrix, visited, matrix[x][y], x+d[0], y+d[1]);
-        }
-    }
-}
+Find all cells where water can flow from that cell to both the Pacific and Atlantic oceans. Return it as a 2D list where each element is a list [r, c] representing the row and column of the cell. You may return the answer in any order.
+
+Example 1:
+Input: heights = [
+  [4,2,7,3,4],
+  [7,4,6,4,7],
+  [6,3,5,3,6]
+]
+
+Output: [[0,2],[0,4],[1,0],[1,1],[1,2],[1,3],[1,4],[2,0]]
+
 """
+
+"""
+1. Brute Force (Backtracking)
+Time complexity: O(m*n*4^(m*n))
+Space complexity: O(m*n)
+"""
+
+
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        ROWS, COLS = len(heights), len(heights[0])
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        pacific = atlantic = False
+
+        def dfs(r, c, prevVal):
+            nonlocal pacific, atlantic
+            if r < 0 or c < 0:
+                pacific = True
+                return
+            if r >= ROWS or c >= COLS:
+                atlantic = True
+                return
+            if heights[r][c] > prevVal:
+                return
+
+            tmp = heights[r][c]
+            heights[r][c] = float("inf")
+            for dx, dy in directions:
+                dfs(r + dx, c + dy, tmp)
+                if pacific and atlantic:
+                    break
+            heights[r][c] = tmp
+
+        res = []
+        for r in range(ROWS):
+            for c in range(COLS):
+                pacific = False
+                atlantic = False
+                dfs(r, c, float("inf"))
+                if pacific and atlantic:
+                    res.append([r, c])
+        return res
+
+
+"""
+2. DFS
+Time complexity: O((m,n))
+Space complexity: O(m*n)
+"""
+
+
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        ROWS, COLS = len(heights), len(heights[0])
+        pac, atl = set(), set()
+
+        def dfs(r, c, visit, prevHeight):
+            if (
+                (r, c) in visit
+                or r < 0
+                or c < 0
+                or r == ROWS
+                or c == COLS
+                or heights[r][c] < prevHeight
+            ):
+                return
+            visit.add((r, c))
+            dfs(r + 1, c, visit, heights[r][c])
+            dfs(r - 1, c, visit, heights[r][c])
+            dfs(r, c + 1, visit, heights[r][c])
+            dfs(r, c - 1, visit, heights[r][c])
+
+        for c in range(COLS):
+            dfs(0, c, pac, heights[0][c])
+            dfs(ROWS - 1, c, atl, heights[ROWS - 1][c])
+
+        for r in range(ROWS):
+            dfs(r, 0, pac, heights[r][0])
+            dfs(r, COLS - 1, atl, heights[r][COLS - 1])
+
+        res = []
+        for r in range(ROWS):
+            for c in range(COLS):
+                if (r, c) in pac and (r, c) in atl:
+                    res.append([r, c])
+        return res
+
+
+"""
+3. BFS
+Time complexity: O((m,n))
+Space complexity: O(m*n)
+"""
+
+
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        ROWS, COLS = len(heights), len(heights[0])
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        pac = [[False] * COLS for _ in range(ROWS)]
+        atl = [[False] * COLS for _ in range(ROWS)]
+
+        def bfs(source, ocean):
+            q = deque(source)
+            while q:
+                r, c = q.popleft()
+                ocean[r][c] = True
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+                    if (
+                        0 <= nr < ROWS
+                        and 0 <= nc < COLS
+                        and not ocean[nr][nc]
+                        and heights[nr][nc] >= heights[r][c]
+                    ):
+                        q.append((nr, nc))
+
+        pacific = []
+        atlantic = []
+        for c in range(COLS):
+            pacific.append((0, c))
+            atlantic.append((ROWS - 1, c))
+
+        for r in range(ROWS):
+            pacific.append((r, 0))
+            atlantic.append((r, COLS - 1))
+
+        bfs(pacific, pac)
+        bfs(atlantic, atl)
+
+        res = []
+        for r in range(ROWS):
+            for c in range(COLS):
+                if pac[r][c] and atl[r][c]:
+                    res.append([r, c])
+        return res
