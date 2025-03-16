4d3be3d15bec7618407fb3328ed19d8eb0cc816d
jc4f <lequan12122002@gmail.com>
2025-03-16 15:09:59 +0700

    207-course-schedule-update

--- leet-code-75/week-3/207-course-schedule.py
+++ leet-code-75/week-3/207-course-schedule.py
@@ -1,22 +1,22 @@
-'''
+"""
 There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
 
 For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
 Return true if you can finish all courses. Otherwise, return false.
 
- 
+
 
 Example 1:
 
 Input: numCourses = 2, prerequisites = [[1,0]]
 Output: true
-Explanation: There are a total of 2 courses to take. 
+Explanation: There are a total of 2 courses to take.
 To take course 1 you should have finished course 0. So it is possible.
 Example 2:
 
 Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
 Output: false
-Explanation: There are a total of 2 courses to take. 
+Explanation: There are a total of 2 courses to take.
 To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
 
 =====>
@@ -45,20 +45,20 @@ This solution is from "Introduction to Algorithms" book where it uses 3 differen
 It's also similar to this article Python DFS + Memoization where we use an array for Memoization.
 
     class Solution:
-    	def buildAdjacencyList(self, n, edgesList):
-    		...
-    		
+        def buildAdjacencyList(self, n, edgesList):
+                ...
+
         def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
             # build Adjacency list from Edges list
             adjList = self.buildAdjacencyList(numCourses, prerequisites)
-    
+
             # Each vertex can have 3 different states:
             # state 0   : vertex is not visited. It's a default state.
             # state -1  : vertex is being processed. Either all of its descendants
             #             are not processed or it's still in the function call stack.
             # state 1   : vertex and all its descendants are processed.
             state = [0] * numCourses
-    
+
             def hasCycle(v):
                 if state[v] == 1:
                     # This vertex is processed so we pass.
@@ -66,22 +66,22 @@ It's also similar to this article Python DFS + Memoization where we use an array
                 if state[v] == -1:
                     # This vertex is being processed and it means we have a cycle.
                     return True
-    
+
                 # Set state to -1
                 state[v] = -1
-    
+
                 for i in adjList[v]:
                     if hasCycle(i):
                         return True
-    
+
                 state[v] = 1
                 return False
-    
+
             # we traverse each vertex using DFS, if we find a cycle, stop and return
             for v in range(numCourses):
                 if hasCycle(v):
                     return False
-    
+
             return True
     42/42 cases passed (96 ms)
     Your runtime beats 96.91 % of python3 submissions
@@ -94,12 +94,12 @@ This technique is also used to find a Topological order from the graph.
     class Solution:
         def buildAdjacencyList(self, n, edgesList):
             ...
-    
+
         def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
             # build Adjacency list from Edges list
             adjList = self.buildAdjacencyList(numCourses, prerequisites)
             visited = set()
-    
+
             def hasCycle(v, stack):
                 if v in visited:
                     if v in stack:
@@ -107,27 +107,64 @@ This technique is also used to find a Topological order from the graph.
                         return True
                     # This vertex is processed so we pass
                     return False
-    
+
                 # mark this vertex as visited
                 visited.add(v)
                 # add it to the current stack
                 stack.append(v)
-    
+
                 for i in adjList[v]:
                     if hasCycle(i, stack):
                         return True
-    
+
                 # once processed, we pop it out of the stack
                 stack.pop()
                 return False
-    
+
             # we traverse each vertex using DFS, if we find a cycle, stop and return
             for v in range(numCourses):
                 if hasCycle(v, []):
                     return False
-    
+
             return True
     42/42 cases passed (100 ms)
     Your runtime beats 92.65 % of python3 submissions
     Your memory usage beats 51.02 % of python3 submissions (16 MB)
-'''
+"""
+
+"""
+1. DFS
+Time complexity: O(V + E)
+Space complexity: O(V + E)
+"""
+
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # Map each course to its prerequisites
+        preMap = {i: [] for i in range(numCourses)}
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+
+        # Store all courses along the current DFS path
+        visiting = set()
+
+        def dfs(crs):
+            if crs in visiting:
+                # Cycle detected
+                return False
+            if preMap[crs] == []:
+                return True
+
+            visiting.add(crs)
+            for pre in preMap[crs]:
+                if not dfs(pre):
+                    return False
+            visiting.remove(crs)
+            preMap[crs] = []
+            return True
+
+        for c in range(numCourses):
+            if not dfs(c):
+                return False
+        return True
