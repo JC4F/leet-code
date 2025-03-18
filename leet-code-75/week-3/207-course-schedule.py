a59c44a9c66862e3b7f7047b9eb0990a1f44c7d1
jc4f <lequan12122002@gmail.com>
2025-03-18 08:52:18 +0700

    update 207-course-schedule-topological

--- leet-code-75/week-3/207-course-schedule.py
+++ leet-code-75/week-3/207-course-schedule.py
@@ -168,3 +168,35 @@ class Solution:
             if not dfs(c):
                 return False
         return True
+
+
+"""
+2. Topological Sort (Kahn's Algorithm)
+Time complexity: O(V + E)
+Space complexity: O(V + E)
+"""
+
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        indegree = [0] * numCourses
+        adj = [[] for i in range(numCourses)]
+        for src, dst in prerequisites:
+            indegree[dst] += 1
+            adj[src].append(dst)
+
+        q = deque()
+        for n in range(numCourses):
+            if indegree[n] == 0:
+                q.append(n)
+
+        finish = 0
+        while q:
+            node = q.popleft()
+            finish += 1
+            for nei in adj[node]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    q.append(nei)
+
+        return finish == numCourses
