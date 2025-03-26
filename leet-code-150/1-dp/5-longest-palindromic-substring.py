982a0eb593555c8d587fa9f0bc72cfa807c56f79
jc4f <lequan12122002@gmail.com>
2025-03-26 09:10:36 +0700

    5-longest-palindromic-substring

---
+++ leet-code-150/1-dp/5-longest-palindromic-substring.py
@@ -0,0 +1,97 @@
+"""
+Description
+Given a string s, return the longest substring of s that is a palindrome.
+
+A palindrome is a string that reads the same forward and backward.
+
+If there are multiple palindromic substrings that have the same length, return any one of them.
+
+Example 1:
+
+Input: s = "ababd"
+
+Output: "bab"
+Explanation: Both "aba" and "bab" are valid answers.
+"""
+
+"""
+1. Brute force
+Time complexity: O(n^3)
+Space complexity: O(n)
+"""
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        res, resLen = "", 0
+
+        for i in range(len(s)):
+            for j in range(i, len(s)):
+                l, r = i, j
+                while l < r and s[l] == s[r]:
+                    l += 1
+                    r -= 1
+
+                if l >= r and resLen < (j - i + 1):
+                    res = s[i : j + 1]
+                    resLen = j - i + 1
+        return res
+
+
+"""
+2. DP
+Time complexity: O(n^2)
+Space complexity: O(n^2)
+"""
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        resIdx, resLen = 0, 0
+        n = len(s)
+
+        dp = [[False] * n for _ in range(n)]
+
+        for i in range(n - 1, -1, -1):
+            for j in range(i, n):
+                if s[i] == s[j] and (j - i <= 2 or dp[i + 1][j - 1]):
+                    dp[i][j] = True
+                    if resLen < (j - i + 1):
+                        resIdx = i
+                        resLen = j - i + 1
+
+        return s[resIdx : resIdx + resLen]
+
+
+"""
+3. Two pointer
+Time complexity: O(n^2)
+Space complexity: O(1)
+"""
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        resIdx = 0
+        resLen = 0
+
+        for i in range(len(s)):
+            # odd length
+            l, r = i, i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    resIdx = l
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+
+            # even length
+            l, r = i, i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    resIdx = l
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+
+        return s[resIdx : resIdx + resLen]
