0dd7c5eb1a6ffc11c656e7180b3889e3c89fce35
jc4f <lequan12122002@gmail.com>
2025-04-02 09:43:50 +0700

    139-word-break-update

--- leet-code-75/week-5/139-work-break.py
+++ leet-code-75/week-5/139-work-break.py
@@ -41,3 +41,108 @@ class Solution:
             return False
 
         return dp(0)
+
+
+"""
+Where n is the length of the string m is the number of words in wordDict and t is the maximum length of any word in wordDict.
+"""
+
+"""
+1. Recursion
+Time complexity: O(t * m^n)
+Space complexity: O(n)
+"""
+
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        def dfs(i):
+            if i == len(s):
+                return True
+
+            for w in wordDict:
+                if (i + len(w)) <= len(s) and s[i : i + len(w)] == w:
+                    if dfs(i + len(w)):
+                        return True
+            return False
+
+        return dfs(0)
+
+
+"""
+2. Dynamic Programming (Bottom-Up)
+Time complexity: O(n * m * t)
+Space complexity: O(n)
+"""
+
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [False] * (len(s) + 1)
+        dp[len(s)] = True
+
+        for i in range(len(s) - 1, -1, -1):
+            for w in wordDict:
+                if (i + len(w)) <= len(s) and s[i : i + len(w)] == w:
+                    dp[i] = dp[i + len(w)]
+                if dp[i]:
+                    break
+
+        return dp[0]
+
+
+"""
+3. DP (Trie)
+Time complexity: O((n * t^2) + m)
+Space complexity: O(n + (m*t))
+"""
+
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_word = False
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_word = True
+
+    def search(self, s, i, j):
+        node = self.root
+        for idx in range(i, j + 1):
+            if s[idx] not in node.children:
+                return False
+            node = node.children[s[idx]]
+        return node.is_word
+
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        trie = Trie()
+        for word in wordDict:
+            trie.insert(word)
+
+        dp = [False] * (len(s) + 1)
+        dp[len(s)] = True
+
+        t = 0
+        for w in wordDict:
+            t = max(t, len(w))
+
+        for i in range(len(s), -1, -1):
+            for j in range(i, min(len(s), i + t)):
+                if trie.search(s, i, j):
+                    dp[i] = dp[j + 1]
+                    if dp[i]:
+                        break
+
+        return dp[0]
