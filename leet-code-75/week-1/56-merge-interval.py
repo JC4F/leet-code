b2c51922121f62a8e200ed63b2a3fae4606d3c2d
JC4F <lequan12122002@gmail.com>
2025-04-06 07:49:34 +0700

    56-merge-interval-update

--- leet-code-75/week-1/56-merge-interval.py
+++ leet-code-75/week-1/56-merge-interval.py
@@ -16,20 +16,23 @@ Explanation: Intervals [1,4] and [4,5] are considered overlapping.
 
 """
 
-from typing import List
+"""
+Sorting
+Time complexity: O(nlogn)
+Space complexity: O(n)
+"""
 
 
-# last number before large than after => merge
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals = sorted(intervals, key=lambda x: x[0])
+        intervals.sort(key=lambda pair: pair[0])
+        output = [intervals[0]]
 
-        ans = []
+        for start, end in intervals:
+            lastEnd = output[-1][1]
 
-        for interval in intervals:
-            if not ans or ans[-1][1] < interval[0]:
-                ans.append(interval)
+            if start <= lastEnd:
+                output[-1][1] = max(lastEnd, end)
             else:
-                ans[-1][1] = max(ans[-1][1], interval[1])
-
-        return ans
+                output.append([start, end])
+        return output
