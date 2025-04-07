eaf4c7205ebef0bda5a38de47c22f4bc57649663
JC4F <lequan12122002@gmail.com>
2025-04-07 08:18:08 +0700

    252-meeting-rooms

---
+++ leet-code-150/intervals/252-meeting-rooms.py
@@ -0,0 +1,39 @@
+"""'
+Given an array of meeting time interval objects consisting of start and end times [[start_1,end_1],[start_2,end_2],...] (start_i < end_i), determine if a person could add all meetings to their schedule without any conflicts.
+
+Example 1:
+
+Input: intervals = [(0,30),(5,10),(15,20)]
+
+Output: false
+Explanation:
+
+(0,30) and (5,10) will conflict
+(0,30) and (15,20) will conflict
+"""
+
+"""
+Sorting
+Time complexity: O(nlogn)
+Space complexity: O(n)
+"""
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        intervals.sort(key=lambda i: i.start)
+
+        for i in range(1, len(intervals)):
+            i1 = intervals[i - 1]
+            i2 = intervals[i]
+
+            if i1.end > i2.start:
+                return False
+        return True
