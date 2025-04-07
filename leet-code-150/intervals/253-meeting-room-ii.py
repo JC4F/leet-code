30e9b6dfd39b375401e0a8491b7f2857de8e942e
JC4F <lequan12122002@gmail.com>
2025-04-07 09:08:31 +0700

    253-meeting-room-ii

---
+++ leet-code-150/intervals/253-meeting-room-ii.py
@@ -0,0 +1,70 @@
+"""
+Given an array of meeting time interval objects consisting of start and end times [[start_1,end_1],[start_2,end_2],...] (start_i < end_i), find the minimum number of days required to schedule all meetings without any conflicts.
+
+Example 1:
+
+Input: intervals = [(0,40),(5,10),(15,20)]
+
+Output: 2
+Explanation:
+day1: (0,40)
+day2: (5,10),(15,20)
+"""
+
+"""
+1. Min Heap
+Time complexity: O(nlogn)
+Space complexity: O(n)
+"""
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        intervals.sort(key=lambda x: x.start)
+        min_heap = []
+
+        for interval in intervals:
+            if min_heap and min_heap[0] <= interval.start:
+                heapq.heappop(min_heap)
+            heapq.heappush(min_heap, interval.end)
+
+        return len(min_heap)
+
+
+"""
+2. Two Pointers
+Time complexity: O(nlogn)
+Space complexity: O(n)
+"""
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        start = sorted([i.start for i in intervals])
+        end = sorted([i.end for i in intervals])
+
+        res = count = 0
+        s = e = 0
+        while s < len(intervals):
+            if start[s] < end[e]:
+                s += 1
+                count += 1
+            else:
+                e += 1
+                count -= 1
+            res = max(res, count)
+        return res
