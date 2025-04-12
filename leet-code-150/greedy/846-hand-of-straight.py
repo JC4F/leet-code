cf68c874ad7b47d1ae7a1355bb933d80feb25c13
JC4F <lequan12122002@gmail.com>
2025-04-12 10:50:10 +0700

    846-hand-of-straight

---
+++ leet-code-150/greedy/846-hand-of-straight.py
@@ -0,0 +1,44 @@
+"""
+You are given an integer array hand where hand[i] is the value written on the ith card and an integer groupSize.
+
+You want to rearrange the cards into groups so that each group is of size groupSize, and card values are consecutively increasing by 1.
+
+Return true if it's possible to rearrange the cards in this way, otherwise, return false.
+
+Example 1:
+
+Input: hand = [1,2,4,2,3,5,3,4], groupSize = 4
+
+Output: true
+Explanation: The cards can be rearranged as [1,2,3,4] and [2,3,4,5].
+"""
+
+"""
+Heap
+Time complexity: O(nlogn)
+Space complexity: O(n)
+"""
+
+
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize:
+            return False
+
+        count = {}
+        for n in hand:
+            count[n] = 1 + count.get(n, 0)
+
+        minH = list(count.keys())
+        heapq.heapify(minH)
+        while minH:
+            first = minH[0]
+            for i in range(first, first + groupSize):
+                if i not in count:
+                    return False
+                count[i] -= 1
+                if count[i] == 0:
+                    if i != minH[0]:
+                        return False
+                    heapq.heappop(minH)
+        return True
